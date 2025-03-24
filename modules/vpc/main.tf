fe8a904a9dbc0179fae9f8adf1ae2d63ee7fd026
chamchimayo <bazzyungfore@gmail.com>
2025-03-24 15:12:47 +0900

    add nat

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -95,6 +95,41 @@ resource "aws_route_table" "public_rt" {
   }
 }
 
+# EIP for NAT Gateway
+resource "aws_eip" "nat_eip" {
+  domain = "vpc"
+
+  tags = {
+    Name = "${var.name}-nat-eip"
+  }
+}
+
+# NAT Gateway (보통 public subnet 1에 배치)
+resource "aws_nat_gateway" "nat" {
+  allocation_id = aws_eip.nat_eip.id
+  subnet_id     = aws_subnet.public_subnet_1.id
+
+  tags = {
+    Name = "${var.name}-nat-gateway"
+  }
+
+  depends_on = [aws_internet_gateway.igw]
+}
+
+# Private Route Table
+resource "aws_route_table" "private_rt" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block     = "0.0.0.0/0"
+    nat_gateway_id = aws_nat_gateway.nat.id
+  }
+
+  tags = {
+    Name = "${var.name}-private-rt"
+  }
+}
+
 resource "aws_route_table_association" "public_assoc_1" {
   subnet_id      = aws_subnet.public_subnet_1.id
   route_table_id = aws_route_table.public_rt.id
@@ -103,4 +138,26 @@ resource "aws_route_table_association" "public_assoc_1" {
 resource "aws_route_table_association" "public_assoc_2" {
   subnet_id      = aws_subnet.public_subnet_2.id
   route_table_id = aws_route_table.public_rt.id
+}
+
+# Associate EKS private subnets with private RT
+resource "aws_route_table_association" "private_eks_assoc_1" {
+  subnet_id      = aws_subnet.private_eks_subnet_1.id
+  route_table_id = aws_route_table.private_rt.id
+}
+
+resource "aws_route_table_association" "private_eks_assoc_2" {
+  subnet_id      = aws_subnet.private_eks_subnet_2.id
+  route_table_id = aws_route_table.private_rt.id
+}
+
+# Associate RDS private subnets with private RT
+resource "aws_route_table_association" "private_db_assoc_1" {
+  subnet_id      = aws_subnet.private_db_subnet_1.id
+  route_table_id = aws_route_table.private_rt.id
+}
+
+resource "aws_route_table_association" "private_db_assoc_2" {
+  subnet_id      = aws_subnet.private_db_subnet_2.id
+  route_table_id = aws_route_table.private_rt.id
 }
