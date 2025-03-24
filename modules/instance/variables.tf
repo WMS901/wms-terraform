fd226a6bb068c64ed93208a8dd8b4b8b0002f65a
chamchimayo <bazzyungfore@gmail.com>
2025-03-24 16:34:16 +0900

    practice 1

---
+++ modules/instance/variables.tf
@@ -0,0 +1,8 @@
+variable "name" {}
+variable "ami_id" {}
+variable "instance_type" {}
+variable "vpc_id" {}
+variable "subnet_id" {}
+variable "public_key_path" {}
+variable "key_name" {}
+variable "my_ip_cidr" {}
