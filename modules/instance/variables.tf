0670483d5cec49cf3a755230949be818e6f1b79a
chamchimayo <bazzyungfore@gmail.com>
2025-03-24 16:37:00 +0900

    delete instance practice

--- modules/instance/variables.tf
+++
@@ -1,8 +0,0 @@
-variable "name" {}
-variable "ami_id" {}
-variable "instance_type" {}
-variable "vpc_id" {}
-variable "subnet_id" {}
-variable "public_key_path" {}
-variable "key_name" {}
-variable "my_ip_cidr" {}
