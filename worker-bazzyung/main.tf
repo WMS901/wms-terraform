9bd56cdccb6aa8d398ff18f344a93f3a5afaebdc
chamchimayo <bazzyungfore@gmail.com>
2025-03-25 15:47:42 +0900

    eks ing..

--- worker-bazzyung/main.tf
+++ worker-bazzyung/main.tf
@@ -10,25 +10,23 @@ data "terraform_remote_state" "vpc" {
 }
 
 module "bastion" {
-  source            = "../modules/bastion"
-  name              = "bazzyung"
-  ami_id            = "ami-0c02fb55956c7d316"
-  instance_type     = "t2.micro"
-  public_subnet_id  = data.terraform_remote_state.vpc.outputs.public_subnet_ids[0]
-  vpc_id            = data.terraform_remote_state.vpc.outputs.vpc_id
-  public_key_path   = "C:/Users/soldesk/Downloads/wms_key.pub"
-  key_name          = "wms-key"
-  my_ip_cidr        = "180.80.107.4/32"
+  source             = "../modules/bastion"
+  name               = "bazzyung"
+  ami_id             = "ami-08b5b3a93ed654d19"
+  instance_type      = "t2.micro"
+  public_subnet_id   = data.terraform_remote_state.vpc.outputs.public_subnet_ids[0]
+  vpc_id             = data.terraform_remote_state.vpc.outputs.vpc_id
+  key_name           = "wms-key"
+  public_key_path    = "C:/Users/soldesk/Downloads/wms_key.pub"  # 로컬 경로
+  my_ip_cidr         = "180.80.107.4/32"
 }
 
 module "eks" {
-  source  = "../modules/eks"
-
-  cluster_name    = "my-wms-cluster"
-  cluster_version = "1.31"
-
-  vpc_id             = data.terraform_remote_state.vpc.outputs.vpc_id
-  private_subnet_ids = data.terraform_remote_state.vpc.outputs.private_eks_subnet_ids
-  key_name           = "wms-key"
+  source              = "../modules/eks"
+  cluster_name        = "my-wms-cluster"
+  cluster_version     = "1.31"
+  vpc_id              = data.terraform_remote_state.vpc.outputs.vpc_id
+  private_subnet_ids  = data.terraform_remote_state.vpc.outputs.private_eks_subnet_ids
+  key_name            = "wms-key"
+  bastion_sg_id       = module.bastion.bastion_sg_id
 }
-
