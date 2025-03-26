8b54f4e4ba551b8830b7fc060d33f1bea68811a6
Hyunw-oo <hyunw9573@gmail.com>
2025-03-26 15:54:12 +0900

    ALB ing...

--- modules/ALB/main.tf
+++ modules/ALB/main.tf
@@ -1,8 +1,19 @@
+# 1. 기존 VPC 상태를 S3에서 가져오기
+data "terraform_remote_state" "vpc" {
+  backend = "s3"
+  config = {
+    bucket = "sol-wms-terraform-states"
+    key    = "vpc/terraform.tfstate"
+    region = "us-east-1"
+  }
+}
+
+# 2. 자동 생성 보안 그룹
 resource "aws_security_group" "this" {
   count       = var.security_group_id == "" ? 1 : 0
   name        = "${var.name}-sg"
   description = "Auto-created SG for ALB"
-  vpc_id      = var.vpc_id
+  vpc_id      = data.terraform_remote_state.vpc.outputs.vpc_id
 
   ingress {
     description = "Allow HTTP from anywhere"
@@ -20,22 +31,25 @@ resource "aws_security_group" "this" {
   }
 }
 
+# 3. ALB 생성
 resource "aws_lb" "this" {
   name               = var.name
   internal           = false
   load_balancer_type = "application"
   security_groups    = [var.security_group_id != "" ? var.security_group_id : aws_security_group.this[0].id]
-  subnets            = var.subnet_ids
+  subnets            = data.terraform_remote_state.vpc.outputs.public_subnet_ids
 }
 
+# 4. Target Group
 resource "aws_lb_target_group" "this" {
   name        = "${var.name}-tg"
   port        = var.target_port
   protocol    = "HTTP"
-  vpc_id      = var.vpc_id
+  vpc_id      = data.terraform_remote_state.vpc.outputs.vpc_id
   target_type = "ip"
 }
 
+# 5. Listener
 resource "aws_lb_listener" "this" {
   load_balancer_arn = aws_lb.this.arn
   port              = 80
--- modules/ALB/variables.tf
+++ modules/ALB/variables.tf
@@ -3,16 +3,6 @@ variable "name" {
   type        = string
 }
 
-variable "subnet_ids" {
-  description = "ALB가 위치할 퍼블릭 서브넷 ID 목록"
-  type        = list(string)
-}
-
-variable "vpc_id" {
-  description = "VPC ID"
-  type        = string
-}
-
 variable "security_group_id" {
   description = "기존 보안 그룹 ID (선택사항). 없으면 자동 생성"
   type        = string
