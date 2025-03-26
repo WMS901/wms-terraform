5eb0fcbf8138b833eb83ecb37df9138e1292914a
gusrnjohn217 <gusrnjohn217@gmail.com>
2025-03-26 17:43:01 +0900

    helmTest

--- worker-hyunku/main.tf
+++ worker-hyunku/main.tf
@@ -6,79 +6,91 @@ provider "aws" {
 # ✅ 기존 VPC 및 Subnet 조회
 ##########################
 
-data "aws_vpc" "selected" {
-  id = "vpc-0fc2f39a043de2120"
+# data "aws_vpc" "selected" {
+#   id = "vpc-0fc2f39a043de2120"
+# }
+
+# data "aws_subnet" "public" {
+#   filter {
+#     name   = "vpc-id"
+#     values = [data.aws_vpc.selected.id]
+#   }
+
+#   filter {
+#     name   = "tag:Name"
+#     values = ["wms-public-subnet-1"]
+#   }
+# }
+
+# data "aws_subnets" "private" {
+#   filter {
+#     name   = "vpc-id"
+#     values = [data.aws_vpc.selected.id]
+#   }
+
+#   filter {
+#     name   = "tag:Name"
+#     values = [
+#       "wms-private-eks-subnet-1",
+#       "wms-private-eks-subnet-2"
+#     ]
+#   }
+# }
+
+module "vpc" {
+  source    = "../modules/vpc"
+  name      = "hyunku"
+  vpc_cidr  = "10.0.0.0/16"
+  azs       = ["us-east-1a", "us-east-1b"]
 }
-
-data "aws_subnet" "public" {
-  filter {
-    name   = "vpc-id"
-    values = [data.aws_vpc.selected.id]
-  }
-
-  filter {
-    name   = "tag:Name"
-    values = ["wms-public-subnet-1"]
-  }
-}
-
-data "aws_subnets" "private" {
-  filter {
-    name   = "vpc-id"
-    values = [data.aws_vpc.selected.id]
-  }
-
-  filter {
-    name   = "tag:Name"
-    values = [
-      "wms-private-eks-subnet-1",
-      "wms-private-eks-subnet-2"
-    ]
-  }
-}
-
-##########################
-# ✅ Bastion EC2 모듈
-##########################
 
 module "bastion" {
   source            = "../modules/bastion"
   name              = "hyunku"
-  ami_id            = "ami-0c02fb55956c7d316"
+  ami_id            = "ami-08b5b3a93ed654d19"
   instance_type     = "t2.micro"
-  public_subnet_id  = data.aws_subnet.public.id
-  vpc_id            = data.aws_vpc.selected.id
+  public_subnet_id  = module.vpc.public_subnet_ids[0]
+  vpc_id            = module.vpc.vpc_id
   my_ip_cidr        = "180.80.107.4/32"
 }
 
-##########################
-# ✅ EKS 모듈
-##########################
-
 module "eks" {
   source  = "../modules/eks"
-
-  cluster_name    = "wms-cluster"
+  cluster_name    = "hyunku-cluster"
   cluster_version = "1.31"
 
-  vpc_id             = data.aws_vpc.selected.id
-  private_subnet_ids = data.aws_subnets.private.ids
-  key_name           = "wms-key"
+  vpc_id             = module.vpc.vpc_id
+  private_subnet_ids = module.vpc.private_eks_subnet_ids
+  key_name           = "wms_key"
+  bastion_sg_id      = module.bastion.bastion_sg_id
 
-  # 자동 cluster creator 권한 부여 비활성화
   enable_cluster_creator_admin_permissions = false
 
-  # EC2 SSM Role에 EKS admin 권한 부여 (✅ 정책 기반 권한 부여)
   access_entries = {
     ec2_ssm_admin = {
-      principal_arn       = "arn:aws:iam::816069155414:role/EC2-SSM"
-      type                = "STANDARD"
-      kubernetes_groups   = []  # ✅ 추가: 필수 항목이므로 빈 배열이라도 넣자
+      principal_arn     = "arn:aws:iam::816069155414:role/EC2-SSM"
+      type              = "STANDARD"
+      kubernetes_groups = []
+
       policy_associations = [
         {
           policy_arn = "arn:aws:eks::aws:cluster-access-policy/AmazonEKSClusterAdminPolicy"
+
+          access_scope = {
+            type = "cluster"
+          }
         }
       ]
     }
   }
+}
+
+resource "aws_security_group_rule" "allow_bastion_to_eks_control_plane" {
+  type                     = "ingress"
+  from_port                = 443
+  to_port                  = 443
+  protocol                 = "tcp"
+  security_group_id        = module.eks.cluster_primary_security_group_id
+  source_security_group_id = module.bastion.bastion_sg_id
+  description              = "Allow Bastion to access EKS API"
 }
